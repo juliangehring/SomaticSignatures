60bcf0c7ae23f59867565f7dbe4afbb5f202d726
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-04-11 21:21:21 +0000

    Second version bump after creating 2.14 release branch.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SomaticSignatures@88840 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/utils.R
@@ -0,0 +1,10 @@
+dfConvertColumns <- function(x, from = "character", to = "factor") {
+    idx = sapply(x, is, from)
+    x[idx] = lapply(x[idx], as, to)
+    return(x)
+}
+
+
+setAs("character", "factor", function(from) {
+    return(as.factor(from))
+})
